de53b86760135a8ec9a8c455939472e976cd021f
AbdelrahmanDwedar <dedou2005@gmail.com>
2024-07-16 21:10:53 +0300

    feat(user): make migration for adding user model with index

---
+++ Migrations/20240706090749_UsersTable.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlogSharp.Migrations
+{
+    /// <inheritdoc />
+    public partial class UsersTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(name: "IX_Users_Id", table: "Users", column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(name: "IX_Users_Id", table: "Users");
+        }
+    }
+}
---
+++ Migrations/20240706102747_AddIsDeletedColumnToUsers.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlogSharp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsDeletedColumnToUsers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "isDeleted",
+                table: "Users",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(name: "isDeleted", table: "Users");
+        }
+    }
+}
