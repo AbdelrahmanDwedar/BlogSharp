e320344146bcfcab70f3ed3c64d690c35bd7c708
AbdelrahmanDwedar <dedou2005@gmail.com>
2025-03-24 20:29:41 +0200

    feat(entities): add Comment entity with properties and constructors; remove old Comments class

---
+++ Entities/Comment.cs
@@ -0,0 +1,34 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace BlogSharp.Entities;
+
+public class Comment
+{
+	public Guid Id { get; set; }
+
+	[Required]
+	public Guid BlogId { get; set; }
+
+	[Required]
+	public Guid UserId { get; set; }
+
+	[Required]
+	[StringLength(500)]
+	public string Content { get; set; }
+
+	public Blog Blog { get; set; }
+	public User User { get; set; }
+
+	// Parameterless constructor for EF Core
+	public Comment() { }
+
+	// Optional constructor for convenience
+	public Comment(Blog blog, User user, string content)
+	{
+		Blog = blog;
+		User = user;
+		Content = content;
+		BlogId = blog.Id;
+		UserId = user.Id;
+	}
+}
